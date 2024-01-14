53dbeeadd7b216d00eae5bdd12d3401dcf398b19
Gabriel Silva Santos <gabrielss432112@gmail.com>
2024-01-14 09:09:01 -0300

    Add styled components

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,6 +3,7 @@ import { Title } from './styled';
 function Login() {
   return (
     <>
+      <Title color='blue'>Hello World!</Title>
       <Title>Hello World!</Title>
     </>
   );
--- src/pages/Login/styled.js
+++ src/pages/Login/styled.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
 
 export const Title = styled.h1`
-  color: red
+  color: ${(prop) => (prop.color ? prop.color : 'pink')}
 `;
