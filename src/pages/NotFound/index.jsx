4c877b00de09bf560cf81ad6ae654df223431fba
Gabriel Silva Santos <gabrielss432112@gmail.com>
2024-02-18 13:34:05 -0300

    🚀 feat(NotFound): Stilish 404 page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
-import { Container, Title } from '../../styles/GlobalStyles';
+import { Container, Title, Subtitle, Row } from '../../styles/GlobalStyles';
+import { Paragraph, HomeLink } from './styled';
 
 const NotFound = () => {
   return (
     <Container>
-      <Title>Página não encontrada</Title>
+      <Title $center>Erro 404 - Página não encontrada!</Title>
+      <Paragraph>
+        Ops, não conseguimos encontrar a página que você estava procurando.
+      </Paragraph>
+      <Row>
+        <HomeLink to='/'>Ir para Página Principal</HomeLink>
+      </Row>
     </Container>
   );
 };
--- src/pages/NotFound/styled.js
+++ src/pages/NotFound/styled.js
@@ -1,5 +1,23 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { disabledDarkColor, primaryColor } from '../../config/colors';
 
-export const Title = styled.h1`
-  color: ${(prop) => (prop.color ? prop.color : 'pink')}
+export const Paragraph = styled.p`
+  color: ${disabledDarkColor};
+  font-size: 1.7rem;
+  padding: 3rem 0;
+  font-weight: bold;
+`;
+
+export const HomeLink = styled(Link)`
+  padding: 1rem 2rem;
+  background-color: ${primaryColor};
+  color: white;
+  border-radius: 6px;
+  transition: all 0.5s ease-in-out;
+  cursor: pointer;
+
+  &:hover {
+    transform: scale(1.1);
+  }
 `;
